93c3d8b210015f2131525f221be29ab872a4c59b
alecishuman <alec@pipeiq.ai>
2024-01-17 15:33:29 -0800

    added projects page

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -1,29 +1,48 @@
-import { useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 
-export default function ProjectCard({ name, descripiton, link }) {
+export default function ProjectCard({ name, description, link }) {
   const card = useRef();
   const hidden = useRef();
-  //   useEffect(() => {
-  //     addEventListener(hoverCard);
-  //     return removeEventListener(hoverCard);
-  //   }, []);
+  const onHover = () => {
+    card.current.style.transform = `translateY(0)`;
+  };
 
-  //   const hoverCard = onmouseover(() => {
-  //     hidden.current.style.setProperty("display", "block");
-  //   });
+  const onLeave = () => {
+    card.current.style.transform = `translateY(${
+      300 - hidden.current.clientHeight
+    }px)`;
+  };
+
+  useEffect(() => {
+    card.current.style.transform = `translateY(${
+      300 - hidden.current.clientHeight
+    }px)`;
+  }, []);
 
   return (
     <div
-      ref={card}
-      className="project-card flex flex-col gap-4 w-60 h-80 rounded-xl p-6 justify-end"
+      className="project-card flex flex-col gap-4 w-72 h-[360px] rounded-xl p-6 overflow-hidden"
+      onMouseEnter={onHover}
+      onMouseLeave={onLeave}
     >
-      <div className="project-title font-semibold">{name}</div>
-      <div ref={hidden} className="project-hide hidden flex flex-col gap-4">
-        <hr />
-        <div className="text-sm">{descripiton}</div>
-        <button className="project-more-btn text-white px-4 py-2 w-fit rounded-lg text-sm">
-          More
-        </button>
+      <div ref={card} className="card-content flex flex-col gap-4">
+        <div ref={hidden} className="project-title font-semibold text-lg">
+          {name}
+        </div>
+        <hr className="bg-gray-400" />
+        <div className="project-hide flex flex-col gap-4">
+          <div className="text-sm">{description}</div>
+          {link && (
+            <a
+              className="project-more-btn text-white px-4 py-2 w-fit rounded-lg text-sm"
+              href={link}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              More
+            </a>
+          )}
+        </div>
       </div>
     </div>
   );
--- pages/api/past-projects.js
+++ pages/api/past-projects.js
@@ -3,12 +3,17 @@ export const pastProjects = [
     name: "Classifying Handwritten Math Symbols",
     description:
       "Conducted and published independent research on classification of math and scientific symbols using neural networks",
-    link: "",
+    link: "https://github.com/alecishuman/Handwritten-Math-Symbols",
   },
   {
     name: "Network Routing Algorithms",
     description:
-      "Analyzed and wrote research paper on various network routing algorithms",
-    link: "",
+      "Analyzed and wrote research paper on various network routing algorithms, comparing and finding the most optimal algorithms.",
+    link: "https://github.com/alecishuman/network-routing-algorithms",
+  },
+  {
+    name: "To Be Continued...",
+    description:
+      "I'm actively developing projects. Come back in a bit to check out all my new stuff!",
   },
 ];
