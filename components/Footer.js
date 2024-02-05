4e2565d1575b5189efd966ab0d4641da51a2e1f6
alecishuman <aasitu@gmail.com>
2024-02-04 23:29:05 -0800

    error fixes

--- components/Footer.js
+++ components/Footer.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
 import gmail from "../public/gmail.svg";
 import linkedin from "../public/linkedin_icon.svg";
 import phone from "../public/phone.svg";
