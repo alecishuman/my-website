96d6b598be990fe7b914b5472e0a73d13dd12e2c
alecishuman <aasitu@gmail.com>
2024-02-07 23:59:45 -0800

    back to top button

--- components/BackToTop.js
+++ components/BackToTop.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import Image from "next/image";
+import upCursor from "../public/up_cursor.svg";
 
 export default function BackToTop() {
   const [isVisible, setIsVisible] = useState(false);
@@ -27,7 +29,7 @@ export default function BackToTop() {
 
   return (
     <button
-      className={"topButton z-20 fixed bottom-5 right-5 rounded-xl cursor-pointer opacity-80 transition-all duration-300 hover:opacity-100 hover:scale-110".concat(
+      className={"topButton z-20 fixed bottom-5 right-5 rounded-2xl cursor-pointer opacity-100 lg:opacity-90 transition-all duration-300 hover:opacity-100 hover:scale-110".concat(
         " ",
         isVisible ? "block" : "hidden"
       )}
@@ -35,7 +37,7 @@ export default function BackToTop() {
         window.scrollTo({ top: 0 });
       }}
     >
-      T
+      <Image src={upCursor} alt="up cursor" width={50} />
     </button>
   );
 }
